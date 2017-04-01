e4cf795ecd88dbf04ac1f4b2df4afe54aabfa07a
Wei Wang <wangwei@comp.nus.edu.sg>
2017-04-02 00:09:54 +0800

    cannot share stop between processes

--- data.py
+++ data.py
@@ -19,7 +19,6 @@ class DataIter(object):
         self.image_size = image_size
         self.capacity = capacity
         self.shuffle = shuffle
-        self.stop = False
         self.proc = None  # for loading triplet
         self.queue = Queue(capacity)
         self.image_pair = []
@@ -55,15 +54,13 @@ class DataIter(object):
 
     def start(self, func):
         # func to be load_triples, load_street_images, load_shop_images
-        self.stop = False
         while not self.queue.empty():
             self.queue.get()
         self.proc = Process(target=func)
         self.proc.start()
 
-    def stop(self):
+    def terminate(self):
         if self.proc is not None:
-            self.stop = True
             time.sleep(0.1)
             self.proc.terminate()
 
@@ -82,8 +79,6 @@ class DataIter(object):
         if self.shuffle:
             random.shuffle(self.idx)
         for b in range(len(self.image_pair) / self.batchsize):
-            if self.stop:
-                return
             if not self.queue.full():
                 qimgs = np.empty((self.batchsize, 3, self.image_size, self.image_size), dtype=np.float32)
                 pimgs = np.empty((self.batchsize, 3, self.image_size, self.image_size), dtype=np.float32)
@@ -106,12 +101,9 @@ class DataIter(object):
                 self.queue.put((qimgs, pimgs, ptags, nimgs, ntags))
             else:
                 time.sleep(0.1)
-        self.stop = True
 
     def load_street_images(self):
         for b in range(len(self.image_pair) / self.batchsize):
-            if self.stop:
-                return
             if not self.queue.full():
                 qimgs = np.empty((self.batchsize, 3, self.image_size, self.image_size), dtype=np.float32)
                 for i, rec in enumerate(self.image_pair[b * self.batchsize: (b + 1) * self.batchsize]):
@@ -121,13 +113,10 @@ class DataIter(object):
                 self.queue.put((qimgs, item))
             else:
                 time.sleep(0.1)
-        self.stop = True
 
     def load_shop_images(self):
         # self.queue = Queue(self.capacity)
         for b in range(len(self.shop_image) / self.batchsize):
-            if self.stop:
-                return
             if not self.queue.full():
                 imgs = np.empty((self.batchsize, 3, self.image_size, self.image_size), dtype=np.float32)
                 tags = np.empty((self.batchsize, self.tag_dim), dtype=np.float32)
@@ -140,7 +129,6 @@ class DataIter(object):
                 self.queue.put((imgs, items, tags))
             else:
                 time.sleep(0.1)
-        self.stop = True
 
 class DARNDataIter(DataIter):
     def __init__(self, image_dir, pair_file, shop_file):
@@ -156,21 +144,17 @@ class FashionDataIter(DataIter):
 
 if __name__ == '__main__':
     image_dir = '/data/jixin/darn_dataset'
-    '''
     train_dat = DARNDataIter(image_dir, './data/darn/train_pair.txt', './data/darn/train_shop.txt')
     train_dat.start(train_dat.load_triples)
     for i in trange(train_dat.num_batches):
         train_dat.next()
-    assert train_dat.stop == True, 'load pairs should stop!'
-    '''
 
     val_dat = DARNDataIter(image_dir, './data/darn/validation_pair.txt', './data/darn/validation_shop.txt')
     val_dat.start(val_dat.load_street_images)
     for i in trange(val_dat.num_batches):
         val_dat.next()
-    assert val_dat.stop == True, 'load street image should stop!'
+    time.sleep(1)
 
     val_dat.start(val_dat.load_shop_images)
     for i in trange(len(val_dat.shop_image) / val_dat.batchsize):
         val_dat.next()
-    assert val_dat.stop == True, 'load shop image should stop!'
